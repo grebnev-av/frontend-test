78d24ec0a24bf074d9b8e0a2227d1d46887f1099
a.grebnev <a.grebnev@smartcat.ai>
2024-11-22 10:40:43 +0300

    added template filling from an object

--- test.js
+++ test.js
@@ -15,55 +15,53 @@
  */
 class Api
 {
-	constructor() 
-	{
+	constructor() {
 
 	}
-
 
 	/**
 	 * Заполняет строковый шаблон template данными из объекта object
 	 *
-	 * @author		User Name 
-	 * @version		v.1.0 (dd/mm/yyyy)
+	 * @author		Grebnev AV
+	 * @version		v.2.0 (dd/mm/yyyy)
 	 * @param		{object} object
 	 * @param		{string} template
 	 * @return		{string}
 	 */
-	get_api_path(object, template)
-	{
-		let result = '';
-
-		/* Здесь ваш код */
-
-		return result;
+	get_api_path(object, template) {
+		return Object.entries(object).reduce(
+			(acc, [key, value]) => acc.replace(new RegExp(`%${key}%`, 'g'), value).replace(/ /g, '%20'), template
+		);
 	}
 }
 
 
-let user =
-{
+const user = {
 	id		: 20,
 	name	: 'John Dow',
 	role	: 'QA',
 	salary	: 100
 };
 
-let api_path_templates =
-[
-	"/api/items/%id%/%name%",
-	"/api/items/%id%/%role%",
-	"/api/items/%id%/%salary%"
+const api_path_templates = [
+	'/api/items/%id%/%name%',
+	'/api/items/%id%/%role%',
+	'/api/items/%id%/%salary%'
 ];
 
-let api = new Api();
+const api = new Api();
 
-let api_paths = api_path_templates.map((api_path_template) =>
-{
+const api_paths = api_path_templates.map((api_path_template) => {
 	return api.get_api_path(user, api_path_template);
 });
 
 console.log(JSON.stringify(api_paths));
 
 // Ожидаемый результат
-let expected_result = ["/api/items/20/John%20Dow","/api/items/20/QA","/api/items/20/100"];
+const expected_result = ['/api/items/20/John%20Dow','/api/items/20/QA','/api/items/20/100'];
+
+function isArraysEqual(firstArray, secondArray) {
+	return firstArray.toString() === secondArray.toString();
+}
+
+console.log('Результат:', isArraysEqual(api_paths, expected_result));
